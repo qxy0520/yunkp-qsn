bf05efdf04585f23ddda4c14f5dd640b5b4e75e2
xqian <xqian@tcd.ie>
2020-07-01 15:10:47 +0100

    云科普青少年版部分网页

---
+++ code.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var myDiv = document.getElementById("div1");
+
+console.log(myDiv);
+
+ myDiv.innerText = "This is the new text";
+//
+ document.getElementById("image1").src = "http://picsum.photos/400";
+//
+// $("div").text("hi there");
+//
+// $("#div2").text("Text of div2");
+//
+// $("#image1").attr("src", "http://picsum.photos/200");
+//
+// $(".aaa").text("HELLO!!");
+//
+// var myLink = $("#searchlink");
+//
+// console.log(myLink);
+//
+// myLink.attr("href", "http://bing.com");
---
+++ converter.js
@@ -0,0 +1,6 @@
+"use strict";
+var mi = prompt("enter a number in miles ");
+document.getElementById("miles").innerHTML = mi;
+var km = mi*1.6;
+document.getElementById("kilometers").innerHTML = km;
+
